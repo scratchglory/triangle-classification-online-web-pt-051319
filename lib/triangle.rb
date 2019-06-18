a5ae1a420702206b47c775ae380a7eb70ba4bf79
Adella <adellakenn@gmail.com>
2019-06-18 21:04:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,7 @@ class Triangle
     @s3 = s3
   end
   
+  
   def kind 
     if (s1*s2*s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s1 + s3) <= s2
         raise TriangleError
@@ -23,9 +24,12 @@ class Triangle
       :scalene
     end
   end
-end
-
+  
 class TriangleError < StandardError
 
 end
+  
+end
+
+
 
